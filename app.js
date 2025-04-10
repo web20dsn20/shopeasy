09d7409b3c6a8463a66561edba93921039347684
santhosh <web20design20@gmail.com>
2025-04-10 14:27:43 +0530

    test bug removed

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const name = "Code Test";
+
 app.get("/", (req, res) => {
-  res.send("Welcome to ShopEasy from CodeTest!");
+  const message = `Welcome to ShopEasy from ${name}!`;
+  res.send(message);
 });
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-module.exports = { app, server };
+module.exports = { app, server,name };
--- test/app.test.js
+++ test/app.test.js
@@ -1,12 +1,12 @@
 const request = require("supertest");
-const { app, server } = require("../app");
+const { app, server, name } = require("../app");
 
 describe("GET /", () => {
   it("should return 200 status and the currect message", async () => {
     const response = await request(app).get("/");
 
     expect(response.status).toBe(200);
-    expect(response.text).toBe("Welcome to ShopEasy from JVLCode!");
+    expect(response.text).toBe(`Welcome to ShopEasy from ${name}!`);
   });
 });
 
