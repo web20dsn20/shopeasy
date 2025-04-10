b5bcb8931fd5965292585d22d074a93722028463
santhosh <web20design20@gmail.com>
2025-04-10 14:17:40 +0530

    add ci pipeline with github actions

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome to ShopEasy from JVLCode");
+  res.send("Welcome to ShopEasy from JVLCode!");
 });
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = { app, server };
