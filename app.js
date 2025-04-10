895bc73da1a95b6e38fc39d276b5b392aa78f164
santhosh <web20design20@gmail.com>
2025-04-10 13:40:13 +0530

    app.js has been modified:Msg changed

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/",(req,res) => {
-    res.send("Welcome to ShopEasy");
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to ShopEasy from JVLCode");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
